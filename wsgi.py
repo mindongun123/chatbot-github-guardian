56422c86195c3318ce1390292eb722b667856a50
nguyenminhdong <mindongun@gmail.com>
2025-06-14 14:40:31 +0700

    deploy wsgi change github_guardian.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,3 @@
-from github_guardian import app  # Nếu đổi tên file thành github_guardian.py
-# Nếu vẫn là github-guardian.py thì phải đổi tên file, vì Python không cho import dấu '-'
-# Nếu chưa đổi tên file, hãy đổi github-guardian.py thành github_guardian.py
-
+from github_guardian import app
 if __name__ == "__main__":
     app.run()
